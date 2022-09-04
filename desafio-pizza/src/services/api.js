8a38ca8cd95fa5caa7c949d2d6c268165f030f95
ademarneto01 <ademarneto47@gmail.com>
2022-09-04 18:20:19 -0300

    commit

--- desafio-pizza/src/services/api.js
+++ desafio-pizza/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://desafio-salt-pizza.herokuapp.com',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
